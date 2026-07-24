5336256d67bfe3ee1cdef364abecd4b365874d97
Shubham Bidve <shubhambidve73@gmail.com>
2026-07-24 10:17:52 +0530

    Day 2: Added basic WHERE queries
    Added SQL queries for practicing WHERE clause.

--- 02_WHERE/WHERE_queries.sql
+++ 02_WHERE/WHERE_queries.sql
@@ -1,1 +1,21 @@
+-- Day 2 : WHERE Clause Practice
 
+-- 1. Display all employees from IT department
+SELECT * FROM Employees
+WHERE Department = 'IT';
+
+-- 2. Display employees whose salary is greater than 50000
+SELECT * FROM Employees
+WHERE Salary > 50000;
+
+-- 3. Display employees from Pune
+SELECT * FROM Employees
+WHERE City = 'Pune';
+
+-- 4. Display employees whose salary is less than 30000
+SELECT * FROM Employees
+WHERE Salary < 30000;
+
+-- 5. Display employees from HR department
+SELECT * FROM Employees
+WHERE Department = 'HR';
